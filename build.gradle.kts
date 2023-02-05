3b4dd38496bbe9360a1344f161f99287330a1ccf
Jaewoong Cheon <cheonjaewoong@gmail.com>
2023-02-05 18:51:56 +0900

    Apply maven publish gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.library") version "7.4.0" apply false
     id("com.android.application") version "7.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("com.vanniktech.maven.publish") version "0.24.0" apply false
 }
 
 allprojects {
