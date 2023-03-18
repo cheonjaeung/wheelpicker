040fefb6a2b1afb04d75f047856fae3322381887
Jaewoong Cheon <cheonjaewoong@gmail.com>
2023-03-18 16:25:20 +0900

    Update samples

--- samples/src/main/java/com/example/snappicker/basicpicker/BasicNumberPickerSample.kt
+++ samples/src/main/java/com/example/snappicker/basicpicker/BasicNumberPickerSample.kt
@@ -2,14 +2,14 @@ package com.example.snappicker.basicpicker
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
-import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
+import com.example.snappicker.R
 import com.example.snappicker.databinding.FragmentBasicNumberPickerSampleBinding
+import com.example.snappicker.databinding.ViewBasicNumberPickerItemBinding
 import io.woong.snappicker.SnapPickerAdapter
 
 public class BasicNumberPickerSample : Fragment() {
@@ -19,26 +19,26 @@ public class BasicNumberPickerSample : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentBasicNumberPickerSampleBinding.inflate(inflater, container, false)
+        val values = (0..99).toList()
+        binding.selectedValue.text = values[0].toString()
         val adapter = BasicNumberPickerAdapter()
-        val values = (0..29).toList()
         adapter.setValues(values)
         binding.picker.adapter = adapter
         binding.picker.setOnValueSelectedListener { _, position ->
-            Log.d("cjw", "value=${values[position]}")
+            binding.selectedValue.text = values[position].toString()
         }
         return binding.root
     }
 }
 
-private class BasicNumberPickerAdapter : SnapPickerAdapter<Int, AppCompatTextView>() {
-    override fun createItemView(context: Context): AppCompatTextView {
-        val textView = AppCompatTextView(context)
-        textView.gravity = Gravity.CENTER
-        return textView
+private class BasicNumberPickerAdapter : SnapPickerAdapter<Int, View>() {
+    override fun createItemView(context: Context): View {
+        val binding = ViewBasicNumberPickerItemBinding.inflate(LayoutInflater.from(context))
+        return binding.root
     }
 
-    override fun bindItemView(itemView: AppCompatTextView, position: Int) {
+    override fun bindItemView(itemView: View, position: Int) {
         val value = getValue(position)
-        itemView.text = value.toString()
+        itemView.findViewById<AppCompatTextView>(R.id.valueText).text = value.toString()
     }
 }
