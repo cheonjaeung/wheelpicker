cadfc307bdaa69d01d2c0970d96271db82843d53
Jaeung Cheon <cheonjaewoong@gmail.com>
2024-09-08 15:48:23 +0900

    Bump gradle to 8.7, AGP to 8.5.1, sample target sdk to 34

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "${project.group}.sample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "${project.group}.sample"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "${project.version}"
     }
