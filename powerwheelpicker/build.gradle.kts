daaace866f25e93da301aa6ce678744860c6bec3
Cheon Jaeung <cheonjaewoong@gmail.com>
2024-11-02 17:13:30 +0900

    Setup maven publish plugin

--- powerwheelpicker/build.gradle.kts
+++ powerwheelpicker/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -26,4 +29,37 @@ dependencies {
     implementation(libs.androidx.appcompat)
     api(libs.androidx.recylerview)
     implementation(libs.simplecarousel)
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+    coordinates("${project.group}", "powerwheelpicker", "${project.version}")
+
+    pom {
+        name.set("PowerWheelPicker for Android")
+        description.set("PowerWheelPicker is a highly customizable wheel picker view for Android.")
+        url.set("https://github.com/cheonjaeung/powerwheelpicker-android")
+
+        licenses {
+            license {
+                name.set("Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("cheonjaeung")
+                name.set("Cheon Jaeung")
+                email.set("cheonjaewoong@gmail.com")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/cheonjaeung/powerwheelpicker-android")
+            connection.set("scm:git:git://github.com/cheonjaeung/powerwheelpicker-android.git")
+            developerConnection.set("scm:git:ssh://git@github.com/cheonjaeung/powerwheelpicker-android.git")
+        }
+    }
 }
