5560423131145091ba81b3008b2cb46f63cf3813
Cheon Jaeung <cheonjaewoong@gmail.com>
2024-11-02 15:24:42 +0900

    Clean up dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.maven.publish) apply false
-    alias(libs.plugins.androidx.navigation.safeargs.kotlin) apply false
 }
 
 allprojects {
